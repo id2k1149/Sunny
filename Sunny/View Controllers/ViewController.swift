e1c1706a609748ec4f8c925bc64217cec91efa58
id2k1149 <id2k1149@gmail.com>
2022-12-18 14:25:53 -0500

    add url and session

--- Sunny/View Controllers/ViewController.swift
+++ Sunny/View Controllers/ViewController.swift
@@ -16,7 +16,20 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let appid = ""
+        
+        let urlString = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=" + appid
+        guard let url = URL(string: urlString) else { return }
+        let session = URLSession(configuration: .default)
+        let task = session.dataTask(with: url) { data, response, error in
+            if let data = data {
+                let dataString = String(data: data, encoding: .utf8)
+                print(dataString!)
+            }
+        }
+        task.resume()
+        
     }
 
     @IBAction func searchPressed(_ sender: UIButton) {
