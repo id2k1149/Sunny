97363b561c207bfac74e5be429d0b1d3347481df
id2k1149 <id2k1149@gmail.com>
2022-12-18 19:14:01 -0500

    add CurrentWeather

---
+++ Sunny/Models/CurrentWeather.swift
@@ -0,0 +1,31 @@
+//
+//  CurrentWeather.swift
+//  Sunny
+//
+//  Created by Max Franz Immelmann on 12/18/22.
+//
+
+import Foundation
+
+struct CurrentWeather {
+    let cityName: String
+    
+    let temperature: Double
+    var temperatureString: String {
+        return "\(temperature.rounded())"
+    }
+    
+    let feelsLikeTemperature: Double
+    var feelsLikeTemperatureString: String {
+        return "\(feelsLikeTemperature.rounded())"
+    }
+    
+    let conditionCode: Int
+    
+    init?(currentWeatherData: CurrentWeatherData) {
+        cityName = currentWeatherData.name
+        temperature = currentWeatherData.main.temp
+        feelsLikeTemperature = currentWeatherData.main.feelsLike
+        conditionCode = currentWeatherData.weather.first!.id
+    }
+}
--- Sunny/Models/NetworkWeatherManager.swift
+++ Sunny/Models/NetworkWeatherManager.swift
@@ -17,24 +17,25 @@ struct NetworkWeatherManager {
             if let data = data {
 //                let dataString = String(data: data, encoding: .utf8)
 //                print(dataString!)
-                
-                self.parseJSON(withData: data)
+                let currentWeather = self.parseJSON(withData: data)
             }
         }
         task.resume()
     }
     
-    func parseJSON(withData data: Data) {
+    func parseJSON(withData data: Data) -> CurrentWeather? {
         let decoder = JSONDecoder()
         
         do {
-            let currentWeatherData = try decoder.decode(CurrentWeatherData.self, from: data)
-            print(currentWeatherData.main.temp)
+            let currentWeatherData = try decoder.decode(CurrentWeatherData.self,
+                                                        from: data)
+//            print(currentWeatherData.main.temp)
+            guard let currentWeather = CurrentWeather(currentWeatherData: currentWeatherData)
+            else { return nil }
+            return currentWeather
         } catch let error as NSError {
             print(error.localizedDescription)
         }
-        
-            
-        
+        return nil
     }
 }
